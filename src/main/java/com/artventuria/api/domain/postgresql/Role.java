83bd131c50e53bcc69b7b2bbb8dd6ce46e80b13d
amine.elalame <elm.dev.code@gmail.com>
2025-05-03 18:59:31 +0200

    feat: add Role model for managing user roles

---
+++ src/main/java/com/artventuria/api/domain/postgresql/Role.java
@@ -0,0 +1,32 @@
+package com.artventuria.api.domain.postgresql;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
