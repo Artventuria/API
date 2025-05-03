1a401aa698193d9e0618dab85becdb9886e71cdb
amine.elalame <elm.dev.code@gmail.com>
2025-05-03 19:54:44 +0200

    feat(repository/jpa): add UserRepository for managing user data

---
+++ src/main/java/com/artventuria/api/repository/jpa/user/UserRepository.java
@@ -0,0 +1,20 @@
+package com.artventuria.api.repository.jpa.user;
+
+import com.artventuria.api.domain.postgresql.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
